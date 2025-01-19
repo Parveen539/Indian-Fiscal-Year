8f4971911af18d49a25b04e181944560b1605084
Parveen Sahrawat <parveen.birohar@gmail.com>
2025-01-19 16:42:27 +0530

    Initial Commit

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IndianFiscalYear = IndianFiscalYear;
-var retrieveFinancialYear_js_1 = require("./src/retrieveFinancialYear.js");
-function IndianFiscalYear(date) {
-    var FinYear = new retrieveFinancialYear_js_1.RetrieveFinancialYear();
-    return FinYear.getFinancialYear(date);
-}
-;
--- index.ts
+++
@@ -1,6 +0,0 @@
-import {RetrieveFinancialYear}  from "./src/retrieveFinancialYear.js";
-
-export function IndianFiscalYear (date : Date) {
-    let FinYear = new RetrieveFinancialYear();
-    return FinYear.getFinancialYear(date);
-};
